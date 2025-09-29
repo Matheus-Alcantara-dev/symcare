8e2a934430af7868172c86b83cfab05ae59812f1
Theuszin-gif <matheusalcantarasc@gmail.com>
2025-09-29 16:47:59 -0300

    Preparando para deploy no Railway

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify
 from models.symcare import SymCare
 
@@ -40,4 +41,4 @@ def pagamento():
     return render_template('pagamento.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
